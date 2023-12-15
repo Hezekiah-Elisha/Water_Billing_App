343945d5acf7716044540efaff5af8a8a3d2fa06
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2023-12-15 12:14:41 +0300

    :construction: Update App Theme.kt

--- app/src/main/java/com/olefaent/waterbilling/MainActivity.kt
+++ app/src/main/java/com/olefaent/waterbilling/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.olefaent.waterbilling.ui.WaterBillingApp
-import com.olefaent.waterbilling.ui.theme.WaterBillingTheme
+import com.olefaent.waterbilling.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WaterBillingTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    WaterBillingTheme {
+    AppTheme {
         WaterBillingApp()
     }
 }
